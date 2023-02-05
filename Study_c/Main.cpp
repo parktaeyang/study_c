afe96f2bb40ba6051683f8482e15c49d9e2e6696
parktaeyang <pty1026@naver.com>
2023-02-05 17:23:00 +0900

    프로젝트 생성.

---
+++ Study_c/Main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: 2023. 2. 5.
+ *      Author: parktaeyang
+ */
+
+#include "Main.h"
+
+Main::Main() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Main::~Main() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Study_c/Main.h
@@ -0,0 +1,17 @@
+/*
+ * Main.h
+ *
+ *  Created on: 2023. 2. 5.
+ *      Author: parktaeyang
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+class Main {
+public:
+	Main();
+	virtual ~Main();
+};
+
+#endif /* MAIN_H_ */
